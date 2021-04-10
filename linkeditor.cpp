cca75e8cc2961af3d499d4c0444275d0f1ab4105
Jacob Le <73577658+jellyjaacob@users.noreply.github.com>
2021-04-09 20:07:53 -0700

    updated link editor file
    made a few methods such as reading a listing file, formatting the estab and others

--- linkeditor.cpp
+++ linkeditor.cpp
@@ -10,17 +10,25 @@
 
 using namespace std;
 
+// global variables
+string ctrl_sect;
+
+
 int main (int argc, int argv[]) {
     
-    // first thing to test is the arguments 
+    // format for command line: led first.sl second.sl
+
+    // test if there are 2+ arguments, led <filename>.sl
     if (argc <= 1) {
         std::cout << "Program needs to have more than 1 argument and should be called with the command of led <filename>.sl" << endl;
         exit(1);
     }
 
+    // test if it is correct file type
     int fileNum = 1;
     string inputFile = argv[fileNum]; // will hold the inputFile
 
+    // loops through a for loop to check the amount of arguments following led are of <filename>.sl
     for (fileNum = 1; fileNum < argc; i++) {
         int exitPos = inputFile.find_last_of(".");
         if (exitPos.substr(exitPos + 1) != "sl") {
@@ -29,6 +37,39 @@ int main (int argc, int argv[]) {
         }
     }
 
-    // need to check for inside the file to see if the contents are correct 
+    // need to check for inside the file to see if the contents are correct
 
 }
+
+void readListingFile() {
+    
+    // read listing file
+    // implementation: search the file for the first instance of the location of 0000, after finding that find the word following
+    // it and instantiate it as the ctrl_sect name
+    // ifstream ctrl_sect()
+
+
+}
+
+// methods to be called inside main
+void estabFormat() {
+
+    string ctrl_sect;
+    int estab_ctrlsectcount = 0;
+    vector <string> estab_ctrlsec;
+
+    for (int i=0;i<estab_ctrlsectcount; i++) {
+        // estab_ctrlsec[i].insert(ctrl_sect + \t + \t + object code)
+    }
+
+
+    int estab_symbolcount = 0;
+    // format the estab
+
+    vector <string> estab_symbol;
+    for (int i=0;i< estab_symbolcount; i++) {
+        // estab_symbol[i].insert( symbol + \t + location of symbol + \t);
+    }
+
+}
+
