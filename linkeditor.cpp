0ab0802c4cf20ae17c0d7352740df64bbbd90788
Jacob Le <jle9629@sdsu.edu>
2021-04-17 19:47:19 -0700

    worked on a function for hex addition that will be used for ESTAB

--- linkeditor.cpp
+++ linkeditor.cpp
@@ -105,14 +105,189 @@ void readListingFile(char* argv[]) {
     string length_of_ls;
 
     while (listFile >> look_for_length) {
-        
+
     }
 
     // formatting ESTAB
-    ESTAB << ctrl_sect_name << "\t\t" << "0000" << endl;
+    ESTAB << ctrl_sect_name << "\t" << "\t" << "000000" << endl;  // need to include address
     for (int j=0;j<addressofseparated.size();j++) {
-        ESTAB << "\t" << separated.at(j) << "\t" << addressofseparated.at(j) << endl;
+        ESTAB << "\t" << separated.at(j) << "\t" << "00" << addressofseparated.at(j) << endl;
     }
 
 }
+/*
+string convertDecimal(string x) {
+
+    // convert hex to decimal
+
+    vector<string> hex_values;
+
+    // 4 digits, ex: 0023, read 0 = 0, then 0 = 0, 2 = 32, 3 = 3, thus 0023 = 35 in binary
+    
+    for (int i = 4; i > 0; i--) {
+        hex_values.at(i) = x[i];
+    }
+
+    for (int j = 0; j < hex_values.max_size(); j++) {
+        // hex_values[j] = convert to binary
+        switch (hex_values.at(j)) {
+            case 'a':
+            case 'A':
+                hex_values.at(j) = '10';
+                break;
+            case 'b':
+            case 'B':
+                hex_values.at(j) = '11';
+                break;
+            case 'c':
+            case 'C':
+                hex_values.at(j) = '12';
+                break;
+            case 'd':
+            case 'D':
+                hex_values.at(j) = '13';
+                break;
+            case 'e':
+            case 'E':
+                hex_values.at(j) = '14';
+                break;
+            case 'f':
+            case 'F':
+                hex_values.at(j) = '15';
+                break;
+        }
+    }
+
+    vector<int> binary_values;
+
+    for (int k = 0; k < hex_values.max_size(); k++ {
+        binary_values.at(k) = hex_values.at(k);         // convert char to int
+    }
+
+    // convert the hex into binary values
+    int power = 3;
+    int hex_power = 16;
+
+    for (int f = 0; f < binary_values.max_size(); f++) {
+        while (power != 0) {
+            hex_power *= power;
+            --power;
+        }
+        binary_values.at(f) *= hex_power;
+    }
+
+    int new_x = 0;
+
+    for (int p = 0; p < binary_values.max_size(); p++) {
+        new_x += binary_values.at(p);
+    }
+    return new_x;
+} */
+
+
+// Map for converting hexadecimal values to decimal
+map<char, int> hex_to_dec()
+{
+	// Map the values to decimal values
+	map<char, int> original_hex { { '0', 0 }, { '1', 1 },
+					{ '2', 2 }, { '3', 3 },
+					{ '4', 4 }, { '5', 5 },
+					{ '6', 6 }, { '7', 7 },
+					{ '8', 8 }, { '9', 9 },
+					{ 'A', 10 }, { 'B', 11 },
+					{ 'C', 12 }, { 'D', 13 },
+					{ 'E', 14 }, { 'F', 15 } };
+
+	return original_hex;
+}
+
+// Map for converting decimal values to hexadecimal
+map<int, char> dec_to_hex()
+{
+	// Map the values to the
+	// hexadecimal values
+	map<int, char> original_hex { { 0, '0' }, { 1, '1' },
+					{ 2, '2' }, { 3, '3' },
+					{ 4, '4' }, { 5, '5' },
+					{ 6, '6' }, { 7, '7' },
+					{ 8, '8' }, { 9, '9' },
+					{ 10, 'A' }, { 11, 'B' },
+					{ 12, 'C' }, { 13, 'D' },
+					{ 14, 'E' }, { 15, 'F' } };
+
+	return original_hex;
+}
+
+
+string hex_addition(string a, string b)
+{
+	map<char, int> m = hex_to_dec();
+	map<int, char> k = dec_to_hex();
+
+	// check which is bigger
+	if (a.length() < b.length())
+		swap(a, b);
+
+	// Store length of both strings
+	int l1 = a.length(), l2 = b.length();
+
+	string ans = "";
+
+	// Initialize carry as zero
+	int carry = 0, i, j;
+
+	// Traverse till second string
+	// get traversal completely
+	for (i = l1 - 1, j = l2 - 1; j >= 0; i--, j--) {
+
+		// Decimal value of element at a[i]
+		// Decimal value of element at b[i]
+		int sum = m[a[i]] + m[b[j]] + carry;
+
+		// Hexadecimal value of sum%16
+		// to get addition bit
+		int addition_bit = k[sum % 16];
+
+		// Add addition_bit to answer
+		ans.push_back(addition_bit);
+
+		// Update carry
+		carry = sum / 16;
+	}
+
+	// Traverse remaining element
+	// of string a
+	while (i >= 0) {
+
+		// Decimal value of element
+		// at a[i]
+		int sum = m[a[i]] + carry;
+
+		// Hexadecimal value of sum%16
+		// to get addition bit
+		int addition_bit = k[sum % 16];
+
+		// Add addition_bit to answer
+		ans.push_back(addition_bit);
+
+		// Update carry
+		carry = sum / 16;
+		i--;
+	}
+
+	// Check if still carry remains
+	if (carry) {
+		ans.push_back(k[carry]);
+	}
+
+	// Reverse the final string
+	// for desired output
+	reverse(ans.begin(), ans.end());
+
+	// Return the answer
+	return ans;
+}
+
+
+
 
